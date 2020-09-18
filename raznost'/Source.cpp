9d5ce061ec43b1efc9e7f7506bbbbbf8c83656b8
Artem Arhipov <artemiy3893@gmail.com>
2020-09-18 21:23:38 +0300

    Start

---
+++ raznost'/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string minuend;
+	string subtrahend;
+	cout << "minuend" << endl;
+	cin >> minuend;
+
+
+	cout << "subtrahend" << endl;
+	cin >> subtrahend;
+
+	return 0;
+}
